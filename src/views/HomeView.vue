5ee3881761935393f3e1ffa3d2f687e787de99d6
Jonathan T Lima <jonathanspadj@gmail.com>
2022-05-12 00:44:48 -0300

    Layout pronto-Navbar e Footer

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main-container">
+  <h1>HOME</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+    }
+}
+</script>
